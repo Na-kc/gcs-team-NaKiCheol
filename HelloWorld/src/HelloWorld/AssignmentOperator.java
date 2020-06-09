8c98f57662740e6d18893b4ce46a054ca41a9f56
Sweep <45086666+HIGHNOOB@users.noreply.github.com>
2020-06-09 11:33:37 +0900

    first project
    yes

---
+++ HelloWorld/src/HelloWorld/AssignmentOperator.java
@@ -0,0 +1,12 @@
+package HelloWorld;
+
+public class AssignmentOperator {
+	public static void main(String[] args) {
+		char[]vowels={'a','e','i','o','u'};
+		//foreach loop
+		for(char vowel:vowels){
+		System.out.print(vowel);
+		}
+	}
+
+}
